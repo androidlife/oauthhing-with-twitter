8983173856d01cd415f35056229928f2e1d317be
laaptu <laaptu@laaptus-MacBook-Pro.local>
2016-04-27 12:49:27 +0545

    Adding info on Preference manager

--- app/src/main/java/com/meg7/soas/oauth/PrefManager.java
+++ app/src/main/java/com/meg7/soas/oauth/PrefManager.java
@@ -3,6 +3,8 @@ package com.meg7.soas.oauth;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import com.meg7.soas.oauth.api.oauth.Token;
+
 /**
  * Created by laaptu on 4/25/16.
  */
@@ -17,6 +19,8 @@ public class PrefManager {
     private static final String AUTH_TOKEN_SECRET = "authTokenSecret";
     private static final String USER_ID = "userId";
     private static final String SCREEN_NAME = "screenName";
+    private static final String USER_PROFILE_IMAGE = "userProfileImage";
+    private static final String USER_PROFILE_BG_IMAGE = "userProfileBgImage";
 
     private static final String SHARED_PREF_NAME = "OAuth";
 
@@ -44,8 +48,43 @@ public class PrefManager {
         editor.apply();
     }
 
+    public Token getAccessToken() {
+        if (!isUserLoggedIn())
+            return null;
+        return new Token(getAuthToken(), getAuthTokenSecret());
+
+    }
+
+    public void storeUserProfileImages(String userProfileImage, String userProfileBgImage) {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(USER_PROFILE_IMAGE, userProfileImage);
+        editor.putString(USER_PROFILE_BG_IMAGE, userProfileBgImage);
+        editor.apply();
+    }
+
+    public String getUserProfileImage() {
+        return sharedPreferences.getString(USER_PROFILE_IMAGE, null);
+    }
+
+    public String getUserScreenName() {
+        return sharedPreferences.getString(SCREEN_NAME, null);
+    }
+
     public boolean isUserLoggedIn() {
-        //
-        return sharedPreferences.getString(AUTH_TOKEN, null) != null;
+        return getAuthToken() != null && getAuthTokenSecret() != null;
+    }
+
+    private String getAuthToken() {
+        return sharedPreferences.getString(AUTH_TOKEN, null);
+    }
+
+    private String getAuthTokenSecret() {
+        return sharedPreferences.getString(AUTH_TOKEN_SECRET, null);
+    }
+
+    public void removeUserCredentials() {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.clear();
+        editor.apply();
     }
 }
